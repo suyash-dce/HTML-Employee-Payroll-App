7490fa97279a96f8fae9f057c75ad27882f080fe
suyash-dce <jain.suyash28@gmail.com>
2020-11-20 17:45:48 +0530

    Updated: Created JSON Object to how Data in Tabular Format

--- JS Scripts/Home Page.js
+++ JS Scripts/Home Page.js
@@ -12,21 +12,57 @@ function createInnerHTML(){
         "<th>Start Date</th>"+
         "<th>Actions</th>";
 
-    const innerHTML=`${headerHTML}
-    <tr>
-        <td><img class="profile" src="./Assets/profile-images/Ellipse -5.png"></td>
-        <td>Narayan Mahadevan</td>
-        <td>Male</td>
-        <td><div class="dept-label">HR</div>
-            <div class="dept-label">FINANCE</div>
-        </td>
-        <td>RS 300000</td>
-        <td>1 Nov 2020</td>
-        <td>
-            <img id="1" onclick="remove()" alt="delete" src="./Assets/icons/delete-black-18dp.svg">
-            <img id="1" onclick="update()" alt="edit" src="./Assets/icons/create-black-18dp.svg">
-        </td>
-    </tr>
-    `;
+    let innerHTML=`${headerHTML}`;
+    let empPayrollList = createEmployeePayrollJson();
+    for(const empData of empPayrollList){
+        innerHTML=`${innerHTML}
+        <tr>
+            <td><img class="profile" src="${empData._profilePic}" alt="Profile Pic"></td>
+            <td>${empData._name}</td>
+            <td>${empData._gender}</td>
+            <td>${getDeptHTML(empData._department)}</td>
+            <td>RS ${empData._salary}</td>
+            <td>${empData._startDate}</td>
+            <td>
+                <img id="${empData._id}" onclick="remove()" alt="delete" src="./Assets/icons/delete-black-18dp.svg">
+                <img id="${empData._id}" onclick="update()" alt="edit" src="./Assets/icons/create-black-18dp.svg">
+            </td>
+        </tr>
+        `;
+    }
     document.querySelector('#table-display').innerHTML = innerHTML;
+}
+
+function getDeptHTML(deptList){
+    let deptHTML = '';
+    for(const dept of deptList){
+        deptHTML = `${deptHTML}<div class="dept-label">${dept}</div>`
+    }
+    return deptHTML;
+}
+
+function createEmployeePayrollJson(){
+    let empPayrollLocalList = [
+        {
+            _name:'Suyash',
+            _gender:'Male',
+            _department:['Engineering'],
+            _salary:'500000',
+            _startDate:'29 Oct 2020',
+            _note:'',
+            _id: new Date().getTime(),
+            _profilePic:'./Assets/profile-images/Ellipse -5.png'
+        },
+        {
+            _name:'Google Assitant',
+            _gender:'Female',
+            _department:['Sales'],
+            _salary:'400000',
+            _startDate:'20 Nov 2020',
+            _note:'',
+            _id: new Date().getTime()+1,
+            _profilePic:'./Assets/profile-images/Ellipse -7.png'
+        }
+    ]
+    return empPayrollLocalList;
 }
